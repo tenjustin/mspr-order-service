20a139ea3cb17e3151d30ab5e699d75e59f1bc81
Bathieuu <mathieu.broccoli@gmail.com>
2025-07-07 16:46:20 +0200

    feat: database

---
+++ src/Kawa.OrderService.Api/Database/Models/OrderLine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kawa.OrderService.Api.Database.Models
+{
+    public class OrderLine
+    {
+        public string OrderId { get; set; } = default!;
+        public string ProductId { get; set; } = default!;
+        public int Quantite { get; set; }
+
+        public Order Order { get; set; } = default!;
+    }
+}
